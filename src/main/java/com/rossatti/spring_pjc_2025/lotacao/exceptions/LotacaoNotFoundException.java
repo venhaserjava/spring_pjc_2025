06c2465276e7ed9efb466cffcb51271c22ea6a0b
venhaserjava <venhaserjava@gmail.com>
2025-03-21 16:09:17 -0400

    feat: Lotacao - criando Repository e exception

---
+++ src/main/java/com/rossatti/spring_pjc_2025/lotacao/exceptions/LotacaoNotFoundException.java
@@ -0,0 +1,13 @@
+package com.rossatti.spring_pjc_2025.lotacao.exceptions;
+
+import com.rossatti.spring_pjc_2025.exceptions.ModelNotFoundException;
+
+public class LotacaoNotFoundException extends ModelNotFoundException {
+
+    public LotacaoNotFoundException(String message) {
+        super(message);
+    }
+    public LotacaoNotFoundException(){
+        super("A Lotação não foi encontrada.");
+    }
+}
---
+++ src/main/java/com/rossatti/spring_pjc_2025/lotacao/repositories/LotacaoRepository.java
@@ -0,0 +1,9 @@
+package com.rossatti.spring_pjc_2025.lotacao.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rossatti.spring_pjc_2025.lotacao.models.Lotacao;
+
+public interface LotacaoRepository  extends JpaRepository<Lotacao,Long>{
+
+}
