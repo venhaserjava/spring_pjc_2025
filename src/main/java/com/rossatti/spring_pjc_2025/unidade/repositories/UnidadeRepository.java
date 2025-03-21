a8ade23cff80c292b7cc25d902d06c692d1eea59
venhaserjava <venhaserjava@gmail.com>
2025-03-20 23:44:17 -0400

    feat: Unidade - Criado Repository

---
+++ src/main/java/com/rossatti/spring_pjc_2025/unidade/repositories/UnidadeRepository.java
@@ -0,0 +1,11 @@
+package com.rossatti.spring_pjc_2025.unidade.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.rossatti.spring_pjc_2025.unidade.models.Unidade;
+
+
+public interface UnidadeRepository extends JpaRepository<Unidade,Long> {
+    Page<Unidade> findByNomeContaining(String nome,Pageable pageable);
+}
