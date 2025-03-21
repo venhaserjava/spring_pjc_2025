ef9e0222868edd9d3daef3e6daf58f207a33bbd2
venhaserjava <venhaserjava@gmail.com>
2025-03-20 23:16:31 -0400

    feat: Commons - Rotas e

---
+++ src/main/java/com/rossatti/spring_pjc_2025/commons/routes/ApiRoutes.java
@@ -0,0 +1,28 @@
+package com.rossatti.spring_pjc_2025.commons.routes;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiRoutes {
+    
+    public static final String API = "/api";
+    public static final String PEOPLE = "/pessoas";    
+    public static final String FIND_PEOPLES = API+PEOPLE;
+    public static final String CREATE_PEOPLE = API+PEOPLE;
+    public static final String UPDATE_PEOPLE = API+PEOPLE+"/{id}";
+    public static final String FIND_PERSON_BY_ID = FIND_PEOPLES +"/{id}";
+
+    public static final String UNITS = "/unidades";
+    public static final String FIND_UNITS = API+UNITS;
+    public static final String FIND_UNIT_BY_ID = FIND_UNITS+"/{id}";
+    public static final String CREATE_UNIT = API+UNITS;
+    public static final String UPDATE_UNIT = API+UNITS+"/{id}";
+
+    public static final String CITIES = "/cidades";
+    public static final String FIND_CITIES = API+CITIES;
+    public static final String FIND_CITY_BY_ID = FIND_CITIES+"/{id}";
+    public static final String CREATE_CITY = API+CITIES;
+    public static final String UPDATE_CITY = API+CITIES+"/{id}";
+
+}
