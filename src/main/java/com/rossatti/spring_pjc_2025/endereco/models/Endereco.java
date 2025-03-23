ef47ddc3487585cdaf836c69a0452a30b2011b26
venhaserjava <venhaserjava@gmail.com>
2025-03-23 11:58:00 -0400

    refactor: Refatoramento da Classe Endereço

--- src/main/java/com/rossatti/spring_pjc_2025/endereco/models/Endereco.java
+++ src/main/java/com/rossatti/spring_pjc_2025/endereco/models/Endereco.java
@@ -6,12 +6,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -23,27 +25,33 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Endereco {
 
     @Id
-    @Column(name = "end_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "end_id")    
     private Long id;
     
     @Column(name = "end_tipo_logradouro",nullable = false, length = 50)
+    @EqualsAndHashCode.Include
     private String tipoLogradouro;
 
     @Column(name = "end_logradouro",nullable = false,length = 200)
+    @EqualsAndHashCode.Include
     private String logradouro;
 
     @Column(name = "end_numero",nullable = false)
+    @EqualsAndHashCode.Include
     private int numero;
     
     @Column(name = "end_bairro",nullable = false,length = 100)
+    @EqualsAndHashCode.Include
     private String bairro;
 
     @ManyToOne
-    @Column(name = "cid_id",nullable = false)
+    @JoinColumn(name = "cid_id", nullable = false)
+    @EqualsAndHashCode.Include
     private Cidade cidade;    
     
 }
