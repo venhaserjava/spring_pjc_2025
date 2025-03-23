672ccd0220a1eb90ed6ea2222c71b778807a4e36
venhaserjava <venhaserjava@gmail.com>
2025-03-23 13:02:39 -0400

    feat: Endereco - implementação do Service

--- src/main/java/com/rossatti/spring_pjc_2025/endereco/services/EnderecoService.java
+++ src/main/java/com/rossatti/spring_pjc_2025/endereco/services/EnderecoService.java
@@ -6,9 +6,9 @@ import com.rossatti.spring_pjc_2025.endereco.dtos.request.EnderecoRequest;
 import com.rossatti.spring_pjc_2025.endereco.dtos.response.EnderecoResponse;
 
 public interface EnderecoService {
-    EnderecoResponse criarEndereco(EnderecoRequest dto);
-    EnderecoResponse buscarPorId(Long id);
-    List<EnderecoResponse> listarTodos();
-    EnderecoResponse atualizarEndereco(Long id, EnderecoRequest dto);
-    void deletarEndereco(Long id);
+    EnderecoResponse create(EnderecoRequest dto);
+    EnderecoResponse findById(Long id);
+    List<EnderecoResponse> findAll();
+    EnderecoResponse update(Long id, EnderecoRequest dto);
+    void delete(Long id);
 }
---
+++ src/main/java/com/rossatti/spring_pjc_2025/endereco/services/EnderecoServiceImpl.java
@@ -0,0 +1,73 @@
+package com.rossatti.spring_pjc_2025.endereco.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.rossatti.spring_pjc_2025.cidade.exceptions.CidadeNotFoundException;
+import com.rossatti.spring_pjc_2025.cidade.models.Cidade;
+import com.rossatti.spring_pjc_2025.cidade.repositories.CidadeRepository;
+import com.rossatti.spring_pjc_2025.endereco.dtos.request.EnderecoRequest;
+import com.rossatti.spring_pjc_2025.endereco.dtos.response.EnderecoResponse;
+import com.rossatti.spring_pjc_2025.endereco.exceptions.EnderecoNotFoundException;
+import com.rossatti.spring_pjc_2025.endereco.mappers.EnderecoMapper;
+import com.rossatti.spring_pjc_2025.endereco.models.Endereco;
+import com.rossatti.spring_pjc_2025.endereco.repositories.EnderecoRepository;
+import jakarta.transaction.Transactional;
+
+public class EnderecoServiceImpl implements EnderecoService {
+    private final EnderecoRepository enderecoRepository;
+    private final CidadeRepository cidadeRepository;
+    private final EnderecoMapper enderecoMapper;
+
+    public EnderecoServiceImpl(EnderecoRepository enderecoRepository, CidadeRepository cidadeRepository, EnderecoMapper enderecoMapper) {
+        this.enderecoRepository = enderecoRepository;
+        this.cidadeRepository = cidadeRepository;
+        this.enderecoMapper = enderecoMapper;
+    }
+
+    @Override
+    @Transactional
+    public EnderecoResponse create(EnderecoRequest dto) {
+        Cidade cidade = cidadeRepository.findById(dto.getCidadeId())
+                .orElseThrow(() -> new CidadeNotFoundException("Cidade não encontrada"));
+        Endereco endereco = enderecoMapper.toModel(dto, cidade);
+        endereco = enderecoRepository.save(endereco);
+        return enderecoMapper.toResponse(endereco);
+    }
+
+    @Override
+    public EnderecoResponse findById(Long id) {
+        Endereco endereco = enderecoRepository.findById(id)
+                .orElseThrow(() -> new EnderecoNotFoundException("Endereço não encontrado"));
+        return enderecoMapper.toResponse(endereco);
+    }
+
+    @Override
+    public List<EnderecoResponse> findAll() {
+        return enderecoRepository.findAll().stream()
+                .map(enderecoMapper::toResponse)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public EnderecoResponse update(Long id, EnderecoRequest dto) {
+        Endereco endereco = enderecoRepository.findById(id)
+                .orElseThrow(() -> new EnderecoNotFoundException("Endereço não encontrado"));
+        Cidade cidade = cidadeRepository.findById(dto.getCidadeId())
+                .orElseThrow(() -> new CidadeNotFoundException("Cidade não encontrada"));
+        endereco = enderecoMapper.toModel(dto, cidade);
+        endereco.setId(id);
+        endereco = enderecoRepository.save(endereco);
+        return enderecoMapper.toResponse(endereco);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        if (!enderecoRepository.existsById(id)) {
+            throw new EnderecoNotFoundException("Endereço não encontrado");
+        }
+        enderecoRepository.deleteById(id);
+    }
+}
