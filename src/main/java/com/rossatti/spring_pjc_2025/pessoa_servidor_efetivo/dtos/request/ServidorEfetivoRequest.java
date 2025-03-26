3c86bb934a865ae4398d31ea5e268e9abea3217e
venhaserjava <venhaserjava@gmail.com>
2025-03-26 18:31:11 -0400

    feat: servidor_efetivo - DTOs

---
+++ src/main/java/com/rossatti/spring_pjc_2025/pessoa_servidor_efetivo/dtos/request/ServidorEfetivoRequest.java
@@ -0,0 +1,26 @@
+package com.rossatti.spring_pjc_2025.pessoa_servidor_efetivo.dtos.request;
+
+import com.rossatti.spring_pjc_2025.lotacao.dtos.LotacaoDTO;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServidorEfetivoRequest {
+
+    @NotNull(message = "O ID da pessoa é obrigatório.")
+    private Long pessoaId;
+
+    @NotNull(message = "matricula é obrigatória e não pode estar vazia ou conter apenas espaços.")
+    private String matricula;
+
+    @NotNull(message = "Os dados de lotação são obrigatórios.")
+    private LotacaoDTO lotacao;    
+
+}
