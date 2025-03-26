d910d504d7b5854b2ece77f1c2002f1701337cf0
venhaserjava <venhaserjava@gmail.com>
2025-03-26 16:19:13 -0400

    feat: servidor_efetivo - classe Entity,Repository

---
+++ src/main/java/com/rossatti/spring_pjc_2025/pessoa_servidor_efetivo/entity/ServidorEfetivo.java
@@ -0,0 +1,39 @@
+package com.rossatti.spring_pjc_2025.pessoa_servidor_efetivo.entity;
+
+import com.rossatti.spring_pjc_2025.pessoa.models.Pessoa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "servidor_efetivo")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServidorEfetivo {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pes_id",nullable = false)
+    private Long id;
+
+    @Column(name = "se_matricula",nullable = false, length = 20)
+    private String matricula;
+
+    @OneToOne
+    @JoinColumn(name = "pes_id", referencedColumnName = "pes_id", insertable = false, updatable = false)
+    private Pessoa pessoa;
+
+}
---
+++ src/main/java/com/rossatti/spring_pjc_2025/pessoa_servidor_efetivo/repositories/ServidorEfetivoRepository.java
@@ -0,0 +1,9 @@
+package com.rossatti.spring_pjc_2025.pessoa_servidor_efetivo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rossatti.spring_pjc_2025.pessoa_servidor_efetivo.entity.ServidorEfetivo;
+
+public interface ServidorEfetivoRepository extends JpaRepository<ServidorEfetivo,Long>{
+
+}
